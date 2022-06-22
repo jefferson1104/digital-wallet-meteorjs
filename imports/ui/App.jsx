b163afd58de5bbe48467146e161133bf3c45fabc
jefferson1104 <jefferson1104junior@gmail.com>
2022-06-22 13:27:14 -0300

    create ContactForm component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { ContactForm } from './ContactForm';
 
 export const App = () => (
   <div>
     <h1>Digital Wallet</h1>
+    <ContactForm />
   </div>
 );
---
+++ imports/ui/ContactForm.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+export const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const saveContact = () => {
+    console.log({ name, email, imageUrl})
+    setName("")
+    setEmail("")
+    setImageUrl("")
+  }
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input 
+          id="name"
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)} 
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input 
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+      </div>
+
+      <div>
+        <label htmlFor="imageUrl">Image URL</label>
+        <input 
+          id="imageUrl" 
+          type="text" 
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)} 
+        />
+      </div>
+
+      <div>
+        <button type="button" onClick={saveContact}>
+          Save Contact
+        </button>
+      </div>
+    </form>
+  )
+}
